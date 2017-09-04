f1df6b729f8338a9300cb94e1c05d8c03bf35d5b
Tom Marek <tm.tomas.marek@gmail.com>
2017-09-04 14:51:06 +0200

    changed to python 3

--- mining_fee_remover.py
+++ mining_fee_remover.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import logging
-import nfqueue
 import os
 import re
 import string
 
 from collections import OrderedDict
+from netfilterqueue import NetfilterQueue
 from scapy.all import *
 
 
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+#logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 class FeeRemover(object):
     SLASH_POOLS = ['nanopool.org','dwarfpool.com']
@@ -24,7 +24,7 @@ class FeeRemover(object):
         self.password = password
         self.worker_suffix = self._format_worker_suffix(pool, worker_name)
 
-        self.logfile = open('fee_remover.log', 'w', 0)
+        #self.logfile = open('fee_remover.log', 'w', 0)
 
     def _format_worker_suffix(self, pool, worker_name):
         worker_suffix = ''
@@ -34,7 +34,7 @@ class FeeRemover(object):
             elif any(s in pool for s in self.DOT_POOLS):
                 worker_suffix = '.' + worker_name
             else:
-                print 'Unknown pool, no worker suffix will be used'
+                print('Unknown pool, no worker suffix will be used')
         return worker_suffix
 
     def _format_wallet_string(self, eth_wallet):
@@ -50,46 +50,36 @@ class FeeRemover(object):
         else:
             raise ValueError('eth_wallet in wrong format')
 
-    def _callback(self, payload):
-        data = payload.get_data()
-        pkt = IP(data)
+    def _callback(self, raw_pkt):
+        pkt = IP(raw_pkt.get_payload())
+        if 'load' in pkt[TCP].payload.fields:
+            payload = pkt[TCP].payload.getfieldval('load')
+            if b'submitLogin' in payload or b'eth_login' in payload:
+                if not bytes(self.eth_wallet, 'utf-8') in payload:
+                    print('Received: "{0}"'.format(payload.decode().strip()))
+                    new_payload = re.sub(b'0x[0-9A-Fa-f]+', bytes(self.eth_wallet, 'utf-8'), payload)
+                    print('Sending: "{0}"'.format(new_payload.decode().strip()))
+                    pkt[TCP].payload.setfieldval('load', new_payload)
 
-        pl_text, pl_len = str(pkt[TCP].payload), len(pkt[TCP].payload)
-        new_pl_text, new_pl_len = pl_text, pl_len
-
-        # edit the payload here
-        if pl_text:
-            if 'eth_submitLogin' in pl_text:
-                if not self.eth_wallet in pl_text:
-                    print 'Received: "{0}"'.format(pl_text.strip())
-                    #new_pl_text = re.sub(r'"0x[0-9A-Fa-f\.\/]+.*?"', '"'+self.eth_wallet+self.worker_suffix+'"', pl_text)
-                    new_pl_text = re.sub(r'0x[0-9A-Fa-f]+', self.eth_wallet, pl_text)
-                    print 'Sending: "{0}"'.format(new_pl_text.strip())
-
-            # update packet
-            pkt[TCP].payload.setfieldval('load', new_pl_text)
-            pkt[IP].len = pkt[IP].len + (len(new_pl_text) - pl_len)
-            pkt[IP].ttl = 64
-            del pkt[IP].chksum
-            del pkt[TCP].chksum
-
-        # update it
-        payload.set_verdict_modified(nfqueue.NF_ACCEPT, str(pkt), len(pkt))
+                    # update packet
+                    #pkt[IP].len = pkt[IP].len + (len(new_pl_text) - pl_len)
+                    pkt[IP].ttl = 64
+                    del pkt[IP].chksum
+                    del pkt[TCP].chksum
+                    raw_pkt.set_payload(bytes(pkt))
+        raw_pkt.accept()
 
     def run(self):
-        print 'Starting mining fee remover...'
+        print('Starting mining fee remover...')
         os.system('iptables -A OUTPUT -p tcp --dport {0} -j NFQUEUE --queue-num 0'.format(int(self.port)))
-        q = nfqueue.queue()
-        q.open()
-        q.set_callback(self._callback)
-        q.create_queue(0)
-        print 'Started.'
+        q = NetfilterQueue()
+        q.bind(0, self._callback)
+        print('Started.')
 
         try:
-            q.try_run()
+            q.run()
         except KeyboardInterrupt:
-            q.unbind(socket.AF_INET)
-            q.close()
+            q.unbind()
         finally:
             os.system('iptables -D OUTPUT -p tcp --dport {0} -j NFQUEUE --queue-num 0'.format(int(self.port)))
 
@@ -97,7 +87,7 @@ if __name__ == "__main__":
     fr = FeeRemover(
         pool='eu1.ethermine.org',
         port=4444,
-        eth_wallet='0xda3e1e7822589a26e9705E184fC340e0731935eA',
+        eth_wallet='e93052e4cdDA79D1C5aDa6c31e15094455aF0eeB',
         password='x',
         #worker_name='wizard2', # TODO: this doesn't work atm because of packet length (probably)
     )
